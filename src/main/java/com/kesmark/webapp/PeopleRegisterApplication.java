cb68732fa0ee78b63773016962f0999b27ce41a4
Kéki Szilveszter <kekiir@gmail.com>
2024-01-31 10:27:37 +0100

    added flyway, mocito, and .env dependencies

---
+++ src/main/java/com/kesmark/webapp/PeopleRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.kesmark.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kesmark/webapp/PeopleRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kesmark.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
